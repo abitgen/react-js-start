2b021e413c82cbbacfc29fc53c955576451d5872
abitgen <abitgenius@gmail.com>
2020-10-22 11:06:06 +0530

    Updates

---
+++ persons-rolodex/precache-manifest.4f1f7264f2f535bce77e9d25e3223986.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5448847a129c63f0afc1fa34c69641",
+    "url": "/react-js-start/persons-rolodex/index.html"
+  },
+  {
+    "revision": "9a47dc36abeff111d276",
+    "url": "/react-js-start/persons-rolodex/static/css/main.221e5c3a.chunk.css"
+  },
+  {
+    "revision": "acf3e36c3c2bb9ac9b15",
+    "url": "/react-js-start/persons-rolodex/static/js/2.47ae780a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-js-start/persons-rolodex/static/js/2.47ae780a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a47dc36abeff111d276",
+    "url": "/react-js-start/persons-rolodex/static/js/main.3a560117.chunk.js"
+  },
+  {
+    "revision": "c72c7e2720a043000c69",
+    "url": "/react-js-start/persons-rolodex/static/js/runtime-main.b5e678dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-js-start/persons-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ precache-manifest.faba16383efdac19f4afcd7858422fa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f7678628f13ea67d75f4dc533b78fd",
+    "url": "/react-js-start/index.html"
+  },
+  {
+    "revision": "8e37e859cbb7af6f48dd",
+    "url": "/react-js-start/static/css/main.221e5c3a.chunk.css"
+  },
+  {
+    "revision": "acf3e36c3c2bb9ac9b15",
+    "url": "/react-js-start/static/js/2.47ae780a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-js-start/static/js/2.47ae780a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e37e859cbb7af6f48dd",
+    "url": "/react-js-start/static/js/main.04dd799f.chunk.js"
+  },
+  {
+    "revision": "7068db82e8825fa1133e",
+    "url": "/react-js-start/static/js/runtime-main.b4d6165e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-js-start/static/media/logo.5d5d9eef.svg"
+  }
+]);
